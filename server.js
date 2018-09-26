6fb2c6db507afb3d72eee94a50dec779f0d199d9
akhilam512 <akhilam512@gmail.com>
2018-09-26 22:11:00 +0530

    commit server

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const request = require('request') 
+
+let apiKey = "37aa1385f3e062fa0c9917966cb0c5b4"
+app.set('view engine', 'ejs')
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true}))
+
+app.get('/', function (req, res) { 
+
+    res.render('weatherhome'), {weather: null, error: null}
+
+
+})
+
+app.post('/', function(req, res) {
+    
+    //console.log(req.body.city)   // bodyParser allows you to access/parse info stored in req, res 
+    
+        let city = req.body.city;
+        let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+      request(url, function (err, response, body) {
+          if(err){
+            res.render('weatherhome', {weather: null, error: 'Error, please try again'});
+          } else {
+            let weather = JSON.parse(body)
+            if(weather.main == undefined){
+              res.render('weatherhome', {weather: null, error: 'Error, please try again'});
+            } else {
+              let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+              res.render('weatherhome', {weather: weatherText, error: null});
+            }
+          }
+        });
+      })
+
+
+
+app.listen(3002, function (){
+    console.log('Testing at port 3002!')
+})
+
+
